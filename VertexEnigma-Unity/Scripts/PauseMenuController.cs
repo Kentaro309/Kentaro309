1079dba79bf2a122bb1ce7e1dc76619bb9f361ff
Kentaro309 <kentaromats@gmail.com>
2025-07-01 16:43:09 -0700

    Update PauseMenuController.cs

--- VertexEnigma-Unity/Scripts/PauseMenuController.cs
+++ VertexEnigma-Unity/Scripts/PauseMenuController.cs
@@ -7,17 +7,6 @@ public class PauseMenuController : MonoBehaviour
 {
 
     public GameObject pauseMenu;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnButtonPressMainMenu()
     {
